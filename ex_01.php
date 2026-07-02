d481df24a3d8d4f360d447ca548dd7db28959ce1
nicolej-amaral <nicole_amaral@estudante.sesisenai.org.br>
2026-07-02 08:11:04 -0300

    add - Final do ex_01

---
+++ ex_01.php
@@ -0,0 +1,25 @@
+<?php
+
+//(x² + y²) / (x+y)
+
+function calcular($x,$y)
+{
+
+    if(($x+$y)== 0){
+
+        return "Não é possível realizar a divisão por zero";
+
+    }
+
+    $resultado = ((pow($x,2)) + pow ($y,2))/($x+$y);
+    return $resultado;
+}
+
+    $x = 10;
+    $y = 5;
+
+    echo "Valor de x é:  $x <br>";
+    echo "Valor de y é:  $y <br>";
+
+    echo "Resultado: " . calcularFormula($x,$y);
+?>
