69173dec516c0acfa9a1ddf89d717cacca81d035
nicolej-amaral <nicole_amaral@estudante.sesisenai.org.br>
2026-07-09 08:10:21 -0300

    add - ex 04 terminado

--- ex_04.php
+++ ex_04.php
@@ -1,9 +1,31 @@
 <?php
 
-funcao gerarsenha($) {
+function gerarSenha($tamanho, $maiusculas, $minusculas, $numeros, $especiais)
+{
+    $maiusculas = "ABCDEFGHIJKLMNOPQRSTUVYXWZ"; // letras maiúsculas
+    $minusculas = "abcdefghijklmnopqrstuvyxwz"; // letras minúsculas
+    $numeros = "0123456789"; // números
+    $especiais = "!@#$%¨&*()_+="; // símbolos
 
-funcao analisartexto($)
+    $senha = "";
 
+    if ($maiusculas) {
+        $senha .= str_shuffle($maiusculas);
+    }
 
+    if ($minusculas) {
+        $senha .= str_shuffle($minusculas);
+    }
 
+    if ($numeros) {
+        $senha .= str_shuffle($numeros);
+    }
+
+    if ($especiais) {
+        $senha .= str_shuffle($especiais);
+    }
+
+    return substr(str_shuffle($senha), 0, $tamanho);
 }
+
+ echo "Senha gerada: " . gerarSenha(12, true, true, true, true);
